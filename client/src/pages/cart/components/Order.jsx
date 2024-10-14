5e95d2968dfbc9fe094fcad8343cbcaf45366d22
dimasgibnev <153992789+dimasgibnev@users.noreply.github.com>
2024-10-14 11:12:26 +0700

    Update Order.jsx

--- client/src/pages/cart/components/Order.jsx
+++ client/src/pages/cart/components/Order.jsx
@@ -1,7 +1,7 @@
 import {  useNavigate } from 'react-router-dom';
 import { Button } from '../../../components/ui';
 import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
-import { selectIsAuth } from '../../../store/slices/authSlice';
+import { selectIsAuth } from '../../../store/slices/userSlice';
 
 import { calculateOrder } from '../../../utils/calculateOrder';
 
